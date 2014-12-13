ccb9904d45cf80d3cb3dc861631ec87e32347f72
Melissa Yung <yung.melissa@gmail.com>
2014-12-13 14:04:12 +0100

    Initial Commit

---
+++ wanderluft/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wanderluft
+//
+//  Created by Melissa Yung on 13/12/14.
+//  Copyright (c) 2014 Travelling Mesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wanderluft/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  wanderluft
+//
+//  Created by Melissa Yung on 13/12/14.
+//  Copyright (c) 2014 Travelling Mesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
